a0b3922161e966b01a08fd3d7740276e470d362e
whispmoe <hello@whisp.moe>
2025-10-25 22:42:26 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simulador de turnos en clínica
+
+## Requisitos
+Antes de ejecutar el programa, asegúrate de instalar las librerías y dependencias necesarias para el proyecto usando el siguiente comando:
+
+```bash
+pip install -r requirements.txt
+```
---
+++ main.py
@@ -0,0 +1,95 @@
+from rich import print
+from rich.text import Text
+from rich.panel import Panel
+
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+from InquirerPy.separator import Separator
+
+NOMBRE_CLINICA = "Clínica Pavas"
+
+
+def registrar_paciente():
+    nombre = inquirer.text(message="Nombre del paciente:").execute()
+
+    if not nombre.strip():
+        print("[bold red]El nombre del paciente no puede estar vació[/bold red]")
+        return
+
+    prioridad = inquirer.select(
+        message="Prioridad del paciente:",
+        choices=[
+            Choice("n", "Normal"),
+            Choice("u", "Urgente"),
+        ],
+    ).execute()
+
+    # Implementación pendiente
+    pass
+
+
+def atender_siguiente_paciente():
+    # Implementación pendiente
+    pass
+
+
+def ver_estado_cola():
+    # Implementación pendiente
+    pass
+
+
+def ver_pacientes_atendidos():
+    # Implementación pendiente
+    pass
+
+
+def calcular_tiempo_promedio():
+    # Implementación pendiente
+    pass
+
+
+def main():
+    print(
+        Panel.fit(
+            f"[bold]{NOMBRE_CLINICA}[/bold] ~ [cyan]Gestión de Pacientes[/cyan]",
+        )
+    )
+
+    while True:
+        seleccion = inquirer.select(
+            message="Elija una opción:",
+            choices=[
+                Choice("registrar", "Registrar paciente"),
+                Choice("atender", "Atender siguiente paciente"),
+                Choice("ver_cola", "Ver estado de la cola"),
+                Choice("ver_pacientes", "Ver pacientes atendidos"),
+                Choice("tiempo_promedio", "Calcular tiempo promedio de atención"),
+                Choice(None, "Salir"),
+            ],
+            default="registrar",
+        ).execute()
+
+        if seleccion == "registrar":
+            registrar_paciente()
+        elif seleccion == "atender":
+            atender_siguiente_paciente()
+        elif seleccion == "ver_cola":
+            ver_estado_cola()
+        elif seleccion == "ver_pacientes":
+            ver_pacientes_atendidos()
+        elif seleccion == "tiempo_promedio":
+            calcular_tiempo_promedio()
+
+        elif seleccion is None:
+            print(f"[bold green]Saliendo del programa, hasta pronto![/bold green]")
+            print()
+            break
+
+        else:
+            print(f"[bold red]Selección no valida[/bold red]")
+
+        print()
+
+
+if __name__ == "__main__":
+    main()
